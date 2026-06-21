f637fb4728e867bfbdcaedcf8f5bb37c3e883f52
TheTagRapper <pskkjk201@outlook.com>
2026-06-21 11:26:18 +0100

    Added input maps for selecting weapons

--- Prototypes/orbiting_system/scenes/orbit_player.gd
+++ Prototypes/orbiting_system/scenes/orbit_player.gd
@@ -4,9 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var weapon_inv = [-1, -1, -1, -1]
+var projectile_count = 3
+var orbit_speed = 4.0
+var orbit_distance = 150
+var time = 0
+
+var weapon_inv = [null, null, null, null]
 var holding_index = 0
-var stack_of_free_indexes = [4, 3, 2, 1]
+var stack_of_free_indexes = [3, 2, 1, 0]
 
 func _physics_process(delta: float) -> void:
 
@@ -19,26 +24,51 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
 		position.y -= SPEED * delta
 	
+	
+	
+	
 	move_and_slide()
 	
+	time += delta
+	
+	# Updating weapon positions
+	for i in range(4):
+		if weapon_inv[i] != null:
+			
+			# Check if it is being held in the hand
+			if holding_index == i:
+				# THIS IS TEMPORARY GUN CODE, WE WILL HANDLE SHOOTING
+				# THIS IS JUST ADDED FOR TESTING
+				var s_width = $Sprite2D.texture.get_width()
+				var s_height = $Sprite2D.texture.get_height()
+				var radius = sqrt((s_width/8) ** 2 + (s_height/8) ** 2)
+				
+				weapon_inv[i].look_at(get_global_mouse_position())
+				weapon_inv[i].position.x = position.x + radius * cos(weapon_inv[i].rotation)
+				weapon_inv[i].position.y = position.y + radius * sin(weapon_inv[i].rotation)
+			
+			# Letting them orbit around
+			else:
+				# Need to make the orbits uniform
+				var orbit_index
+				if i == 3:
+					orbit_index = holding_index
+				else:
+					orbit_index = i
+				
+				weapon_inv[i].position.x = position.x + cos(2*PI/projectile_count * i + time * orbit_speed) * orbit_distance
+				weapon_inv[i].position.y = position.y + sin(2*PI/projectile_count * i + time * orbit_speed) * orbit_distance
+
 
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	
-	
 	# Checking Collisions
 	if area.is_in_group("WEAPON"):
 		add_weapon(area)
 		
-	for i in range(4):
-		weapon = weapon_inv[i]
-		
-		if weapon != -1:
-			
-			# Check if it is being held in the hand
-			if holding_index == i:
-				
+
+	
 
 func add_weapon(area: Area2D):
 	
@@ -47,13 +77,18 @@ func add_weapon(area: Area2D):
 	# Checking if full
 	if no_of_weapons >= 0 and no_of_weapons < 4:		
 		var weapon_index = stack_of_free_indexes.pop_back()
-		weapon_inv[weapon_index] = area.owner
+		weapon_inv[weapon_index] = area.get_parent()
 		
 		# Is in the 
 		if no_of_weapons == 0:
 			holding_index = weapon_index
+		
+		area.get_parent().add_index(weapon_index)
 
-func remove_weapon(weapon : Node2D , weapon_index : int):
+func remove_weapon(weapon : Marker2D , weapon_index : int):
 	weapon.break_now()
-	weapon_inv[weapon_index] = -1
+	weapon_inv[weapon_index] = null
 	stack_of_free_indexes.push_back(weapon_index)
+
+func swap_weapons(selected_index : int):
+	holding_index = selected_index
---
+++ Prototypes/orbiting_system/scenes/temp_orbit_weapon.gd
@@ -0,0 +1,14 @@
+extends Marker2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label.text = "no owner"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func add_index(index):
+	$Label.text = str(index)
