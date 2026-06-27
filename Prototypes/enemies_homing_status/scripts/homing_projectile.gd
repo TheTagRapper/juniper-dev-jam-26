0c26049957b2d7a12764b22fd2e4c87020dfd07a
waterdude551 <waterdude551@gmail.com>
2026-06-26 23:14:38 -0400

    level 3 wip

--- Prototypes/enemies_homing_status/scripts/homing_projectile.gd
+++ Prototypes/enemies_homing_status/scripts/homing_projectile.gd
@@ -24,24 +24,27 @@ func rotate_to_target():
 
 func move():
 	if direction:
-		position += direction * speed;
+		global_position += direction * speed;
 
-func check_bounds(): # change for room
-	if (global_position.x < 0 || global_position.y < 0 
-		|| global_position.x > get_viewport_rect().size.x 
-		|| global_position.y > get_viewport_rect().size.y):
-		queue_free()
+#func check_bounds(): # change for room
+	#if (global_position.x < 0 || global_position.y < 0 
+		#|| global_position.x > get_viewport_rect().size.x 
+		#|| global_position.y > get_viewport_rect().size.y):
+		#queue_free()
 
 func _physics_process(_delta: float) -> void:
 	if homing:
 		rotate_to_target()
 	move()
-	check_bounds()
+	#check_bounds()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.take_damage(dmg)
+	if body.is_in_group("enemy"):
+		return
 	var parent = get_parent()
 	if parent != null:
+		print("Homing hit " + body.name)
 		get_parent().queue_free()
